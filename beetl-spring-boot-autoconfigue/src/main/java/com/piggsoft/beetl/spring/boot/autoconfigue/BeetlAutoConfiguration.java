44f098fea9297e0ce443ef63085a2df2ecd7dd8a
yaochen4 <yaochen4@iflytek.com>
2016-11-30 17:38:40 +0800

    init

---
+++ beetl-spring-boot-autoconfigue/src/main/java/com/piggsoft/beetl/spring/boot/autoconfigue/BeetlAutoConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * Copyright (C) 1999-2016 IFLYTEK Inc.All Rights Reserved.
+ * History：
+ * Version   Author      Date                              Operation
+ * 1.0       yaochen4    2016/11/30                           Create
+ */
+package com.piggsoft.beetl.spring.boot.autoconfigue;
+
+import org.beetl.core.resource.ClasspathResourceLoader;
+import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
+import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yaochen4
+ * @version 1.0
+ * @create 2016/11/30
+ * @since 1.0
+ */
+@Configuration
+@EnableConfigurationProperties(BeetlProperties.class)
+public class BeetlAutoConfiguration {
+
+
+    @Bean(initMethod = "init", name = "beetlConfig")
+    public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
+        BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
+        try {
+            ClasspathResourceLoader cploder = new ClasspathResourceLoader(BeetlAutoConfiguration.class.getClassLoader(), "templatesPath");
+            beetlGroupUtilConfiguration.setResourceLoader(cploder);
+            return beetlGroupUtilConfiguration;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Bean(name = "beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(@Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+        BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
+        beetlSpringViewResolver.setOrder(0);
+        beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
+        return beetlSpringViewResolver;
+    }
+
+}
---
+++ beetl-spring-boot-autoconfigue/src/main/java/com/piggsoft/beetl/spring/boot/autoconfigue/BeetlProperties.java
@@ -0,0 +1,23 @@
+/*
+ *
+ * Copyright (C) 1999-2016 IFLYTEK Inc.All Rights Reserved.
+ * History：
+ * Version   Author      Date                              Operation
+ * 1.0       yaochen4    2016/11/30                           Create
+ */
+package com.piggsoft.beetl.spring.boot.autoconfigue;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author yaochen4
+ * @version 1.0
+ * @create 2016/11/30
+ * @since 1.0
+ */
+@ConfigurationProperties(prefix = BeetlProperties.BEETL_PREFIX)
+public class BeetlProperties {
+
+    public static final String BEETL_PREFIX = "beetl";
+
+}
