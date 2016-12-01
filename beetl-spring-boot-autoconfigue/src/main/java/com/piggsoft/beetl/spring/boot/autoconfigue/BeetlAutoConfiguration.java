4d294e2ff00b7aa6f1fa86896654285ea2177137
piggsoft <piggsoft@163.com>
2016-12-01 22:10:54 +0800

    modify configurations

--- beetl-spring-boot-autoconfigue/src/main/java/com/piggsoft/beetl/spring/boot/autoconfigue/BeetlAutoConfiguration.java
+++ beetl-spring-boot-autoconfigue/src/main/java/com/piggsoft/beetl/spring/boot/autoconfigue/BeetlAutoConfiguration.java
@@ -7,13 +7,20 @@
  */
 package com.piggsoft.beetl.spring.boot.autoconfigue;
 
+import org.beetl.core.*;
 import org.beetl.core.resource.ClasspathResourceLoader;
 import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
 import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author yaochen4
@@ -25,17 +32,101 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(BeetlProperties.class)
 public class BeetlAutoConfiguration {
 
+    @Autowired
     private BeetlProperties properties;
 
-    public BeetlAutoConfiguration(BeetlProperties properties) {
-        this.properties = properties;
-    }
+    /**
+     * 自定义函数
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlFunctions")
+    private Map<String, Function> functions = null;
+    /**
+     * 自定义类型默认格式化器
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlTypeFormats")
+    private Map<Class<?>, Format> typeFormats = null;
+    /**
+     * 自定义格式化器
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlFormats")
+    private Map<String, Format> formats = null;
+    /**
+     * 自定义标签
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlTagFactorys")
+    private Map<String, TagFactory> tagFactorys = null;
+    /**
+     * 自定义函数包
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlFunctionPackages")
+    private Map<String, Object> functionPackages = null;
+    /**
+     * 自定义虚拟属性
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlVirtualClassAttributes")
+    private Map<Class<?>, VirtualClassAttribute> virtualClassAttributes = null;
+    /**
+     * 自定义虚拟属性执行器
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlVirtualAttributeEvals")
+    private List<VirtualAttributeEval> virtualAttributeEvals = null;
+    /**
+     * 异常处理器
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlErrorHandler")
+    protected ErrorHandler errorHandler = null;
+    /**
+     * 共享变量
+     */
+    @Autowired(required = false)
+    @Qualifier("beetlSharedVars")
+    protected Map<String, Object> sharedVars = null;
+
 
     @Bean(initMethod = "init", name = "beetlConfig")
     public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
         BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
         try {
             ClasspathResourceLoader cploder = new ClasspathResourceLoader(BeetlAutoConfiguration.class.getClassLoader(), properties.getTemplatesPath());
+            if (properties.getConfig() != null) {
+                beetlGroupUtilConfiguration.setConfigProperties(properties.getConfig().toProperties());
+            }
+            if (functions != null) {
+                beetlGroupUtilConfiguration.setFunctions(functions);
+            }
+             if (typeFormats != null) {
+                beetlGroupUtilConfiguration.setTypeFormats(typeFormats);
+            }
+             if (formats != null) {
+                beetlGroupUtilConfiguration.setFormats(formats);
+            }
+             if (tagFactorys != null) {
+                beetlGroupUtilConfiguration.setTagFactorys(tagFactorys);
+            }
+             if (functionPackages != null) {
+                beetlGroupUtilConfiguration.setFunctionPackages(functionPackages);
+            }
+             if (virtualClassAttributes != null) {
+                beetlGroupUtilConfiguration.setVirtualClassAttributes(virtualClassAttributes);
+            }
+             if (virtualAttributeEvals != null) {
+                beetlGroupUtilConfiguration.setVirtualAttributeEvals(virtualAttributeEvals);
+            }
+             if (errorHandler != null) {
+                beetlGroupUtilConfiguration.setErrorHandler(errorHandler);
+            }
+             if (sharedVars != null) {
+                beetlGroupUtilConfiguration.setSharedVars(sharedVars);
+            }
+
             beetlGroupUtilConfiguration.setResourceLoader(cploder);
             return beetlGroupUtilConfiguration;
         } catch (Exception e) {
--- beetl-spring-boot-example/src/main/java/com/piggsoft/BeetlSpringBootExampleApplication.java
+++ beetl-spring-boot-example/src/main/java/com/piggsoft/BeetlSpringBootExampleApplication.java
@@ -1,10 +1,21 @@
 package com.piggsoft;
 
+import org.beetl.core.VirtualClassAttribute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class BeetlSpringBootExampleApplication {
+
+	@Bean(name = "beetlVirtualClassAttributes")
+	public Map<Class<?>, VirtualClassAttribute> virtualClassAttributeMap() {
+		Map<Class<?>, VirtualClassAttribute> map = new HashMap<Class<?>, VirtualClassAttribute>();
+		return map;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeetlSpringBootExampleApplication.class, args);
